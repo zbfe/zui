d72d78f9f253f6243639765f48b5cc4c76513f39
xuexb <fe.xiaowu@gmail.com>
2016-05-08 22:55:29 +0800

    update: 添加编译注释

--- build-gh-pages.js
+++ build-gh-pages.js
@@ -1,3 +1,12 @@
+/**
+ * @file 编译gh-pages脚本，目前只是简单的copy文件
+ * @author fe.xiaowu@gmail.com
+ * @todo
+ *     1. 文件编译：混淆、合并、压缩
+ *     2. 编译`docs`文件
+ *     3. 自动生成demo路径和图
+ */
+
 var child = require('child_process');
 
 console.log('build start!');
@@ -8,6 +17,7 @@ child.execSync([
     'cp -rf lib gh-pages/',
     'cp -rf src gh-pages/',
     'cp -rf test gh-pages/',
-    'cp -rf index.html gh-pages/'
+    'cp -rf index.html gh-pages/',
+    'cp -rf README.md gh-pages/'
 ].join(' && '));
 console.log('build end!');
