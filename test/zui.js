e9c7e982298a948da39c90e2a872b6f734cd2af0
xuexb <fe.xiaowu@gmail.com>
2016-06-03 09:08:42 +0800

    update: 更新zui测试用例100%

--- test/zui.js
+++ test/zui.js
@@ -26,8 +26,7 @@ define([
      *
      * @type {Array}
      */
-    var types = emptyTypes.concat([
-        function () {},
+    var types = emptyTypes.concat([function () {},
         /1/,
         '1',
         1,
@@ -99,11 +98,14 @@ define([
             expect(app.get('c')).toBe(3);
         });
 
-        it('on check param and return', function () {
+        it('.on,.one,.off,.trigger,.set check param and return', function () {
             var app = new Zui();
 
             checkParam(function (key, key2) {
                 expect(app.on(key, key2)).toEqual(app);
+                expect(app.one(key, key2)).toEqual(app);
+                expect(app.off(key, key2)).toEqual(app);
+                expect(app.trigger(key, key2)).toEqual(app);
             });
         });
 
@@ -149,7 +151,6 @@ define([
             app.trigger('callback');
         });
 
-
         it('on callback param', function (done) {
             var app = new Zui();
 
@@ -164,7 +165,9 @@ define([
 
             app.on('callback2', function (a) {
                 expect(a).toBe(1);
-            }).trigger('callback2', [1]);
+            }).trigger('callback2', [
+                1
+            ]);
 
             app.on('callback3', function (a) {
                 expect(a).toEqual({});
@@ -174,25 +177,27 @@ define([
                 expect(a).toBe(1);
                 expect(b).toBe(2);
                 expect(c).toBe(3);
-            }).trigger('callback4', [1, 2, 3]);
+            }).trigger('callback4', [
+                1,
+                2,
+                3
+            ]);
 
             app.on('callback5', function (a) {
                 expect(a.a).toBe(1);
-            }).trigger('callback5', {a: 1});
+            }).trigger('callback5', {
+                a: 1
+            });
 
             app.on('callback6', function (a) {
                 expect(a.a).toBe(1);
-            }).trigger('callback6', [{a: 1}]);
+            }).trigger('callback6', [
+                {
+                    a: 1
+                }
+            ]);
 
             setTimeout(done);
-        });
-
-        it('one check param and return', function () {
-            var app = new Zui();
-
-            checkParam(function (key, key2) {
-                expect(app.on(key, key2)).toEqual(app);
-            });
         });
 
         it('one event', function (done) {
@@ -237,7 +242,6 @@ define([
             app.trigger('callback');
         });
 
-
         it('one callback param', function (done) {
             var app = new Zui();
 
@@ -252,7 +256,9 @@ define([
 
             app.one('callback2', function (a) {
                 expect(a).toBe(1);
-            }).trigger('callback2', [1]);
+            }).trigger('callback2', [
+                1
+            ]);
 
             app.one('callback3', function (a) {
                 expect(a).toEqual({});
@@ -262,17 +268,220 @@ define([
                 expect(a).toBe(1);
                 expect(b).toBe(2);
                 expect(c).toBe(3);
-            }).trigger('callback4', [1, 2, 3]);
+            }).trigger('callback4', [
+                1,
+                2,
+                3
+            ]);
 
             app.one('callback5', function (a) {
                 expect(a.a).toBe(1);
-            }).trigger('callback5', {a: 1});
+            }).trigger('callback5', {
+                a: 1
+            });
 
             app.one('callback6', function (a) {
                 expect(a.a).toBe(1);
-            }).trigger('callback6', [{a: 1}]);
+            }).trigger('callback6', [
+                {
+                    a: 1
+                }
+            ]);
 
             setTimeout(done);
+        });
+
+        it('.off(key, callback)', function () {
+            var app = new Zui();
+            var num = 0;
+
+            var fn = function () {
+                num += 1;
+            };
+
+            app.on('1', fn);
+
+            app.one('1 1', function () {
+                num += 1;
+            });
+
+            app.one('1', function () {
+                num += 1;
+            });
+
+            app.off('1', fn);
+            app.off('1', function () {});
+
+            app.trigger('1');
+            app.trigger('1');
+            app.trigger('1');
+
+            expect(num).toBe(3);
+        });
+
+        it('.off(key)', function () {
+            var app = new Zui();
+            var num = 0;
+
+            app.on('1', function () {
+                num += 1;
+            });
+
+            app.one('1 1', function () {
+                num += 1;
+            });
+
+            app.off('1');
+
+            app.trigger('1');
+
+            expect(num).toBe(0);
+        });
+
+        it('.trigger(key)', function (done) {
+            var app = new Zui();
+
+            app.on('1', function (a) {
+                expect(a).toBeUndefined();
+                done();
+            });
+
+            app.trigger('1');
+        });
+
+        it('.trigger(key, str)', function (done) {
+            var app = new Zui();
+
+            app.on('1', function (a, b) {
+                expect(a).toBe('str');
+                expect(b).toBeUndefined();
+                done();
+            });
+
+            app.trigger('1', 'str');
+        });
+
+        it('.trigger(key, num)', function (done) {
+            var app = new Zui();
+
+            app.on('1', function (a, b) {
+                expect(a).toBe(1);
+                expect(b).toBeUndefined();
+                done();
+            });
+
+            app.trigger('1', 1);
+        });
+
+        it('.trigger(key, {})', function (done) {
+            var app = new Zui();
+
+            app.on('1', function (a, b) {
+                expect(a).toEqual({
+                    a: 1
+                });
+                expect(b).toBeUndefined();
+                done();
+            });
+
+            app.trigger('1', {
+                a: 1
+            });
+        });
+
+        it('.trigger(key, [1, 2])', function (done) {
+            var app = new Zui();
+
+            app.on('1', function (a, b, c) {
+                expect(a).toBe(1);
+                expect(b).toBe(2);
+                expect(c).toBeUndefined();
+                done();
+            });
+
+            app.trigger('1', [
+                1,
+                2
+            ]);
+        });
+
+        it('.trigger this', function (done) {
+            var app = new Zui();
+
+            app.on('1', function () {
+                expect(this).toEqual(app);
+                done();
+            });
+
+            app.trigger('1');
+        });
+
+        it('.trigger(key, [1, 2])', function (done) {
+            var app = new Zui();
+            var num = 0;
+
+            app.on('1', function () {
+                num += 1;
+            });
+
+            app.trigger('1').trigger('1');
+
+            expect(num).toBe(2);
+            setTimeout(done);
+        });
+
+        // get && set
+        it('.get', function () {
+            var a = {
+                a: 1
+            };
+            var app = new Zui(a);
+            expect(app.get()).toEqual(a);
+
+            app = new Zui();
+            expect(app.get()).toEqual({});
+        });
+
+        it('.get(key)', function () {
+            var a = {
+                a: 1
+            };
+            var app = new Zui(a);
+            expect(app.get('a')).toBe(1);
+            expect(app.get('b')).toBeUndefined();
+        });
+
+        it('.set', function () {
+            var app = new Zui();
+
+            app.set('a', 1);
+            app.set('b', undefined);
+
+            expect(app.get('a')).toBe(1);
+            expect(app.get('b')).toBeUndefined();
+        });
+
+        // _
+        it('._getListener', function () {
+            var app = new Zui();
+
+            expect(Array.isArray(app._getListener('1'))).toBe(true);
+        });
+
+        it('._access', function () {
+            var app = new Zui();
+            var data = {};
+
+            app._access('1 2  3  4  ', function (key) {
+                data[key] = true;
+            });
+
+            expect(data).toEqual({
+                '1': true,
+                '2': true,
+                '3': true,
+                '4': true
+            });
         });
     });
 });
