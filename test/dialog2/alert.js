57bff3768f76f0bdce9b522cbf9667c41d35aec5
xuexb <fe.xiaowu@gmail.com>
2016-06-08 12:32:50 +0800

    add: 添加测试

---
+++ test/dialog2/alert.js
@@ -0,0 +1,34 @@
+/**
+ * @file 弹出层测试
+ * @author fe.xiaowu@gmail.com
+ */
+
+define([
+    'dialog2/alert',
+    'zepto'
+], function (Alert, $) {
+    'use strict';
+
+    describe('dialog2/alert', function () {
+        afterEach(function () {
+            $('.zui-dialog').remove();
+        });
+
+        it('Alert(title)', function () {
+            new Alert('标题');
+            expect($('.zui-dialog').length).toBe(1);
+        });
+
+        it('Alert(title, callback)', function (done) {
+            var app = new Alert('标题', done);
+            expect($('.zui-dialog').length).toBe(1);
+            app.$wrap.find('.zui-dialog-button').triggerHandler('click');
+        });
+
+        it('Alert(title, content, callback)', function (done) {
+            var app = new Alert('标题', '内容', done);
+            expect($('.zui-dialog').length).toBe(1);
+            app.$wrap.find('.zui-dialog-button').triggerHandler('click');
+        });
+    });
+});
