5704e587de4d269689be7664fa47514f932e6eb5
schoeu <quanyou1013@gmail.com>
2016-06-20 22:26:17 +0800

    update dialog test case

--- test/dialog/dialog.js
+++ test/dialog/dialog.js
@@ -12,12 +12,35 @@ define([
         afterEach(function () {
             $('.zui-dialog-musk').remove();
         });
-    });
 
-    it('Dialog(content)', function () {
-        var app = new Dialog('内容');
-        expect($('.zui-dialog-wrap').length).toBe(1);
-        app.ele.find('.zui-dialog-btns').triggerHandler('click');
-    });
+        it('Dialog("content")', function () {
+            var app = new Dialog('内容');
+            expect($('.zui-dialog-wrap').length).toBe(1);
+            app.ele.find('.zui-dialog-btns').triggerHandler('click');
+        });
 
+        it('Dialog({content:"content"})', function () {
+            var app = new Dialog({content: '内容'});
+            expect($('.zui-dialog-wrap').length).toBe(1);
+            app.ele.find('.zui-dialog-btns').triggerHandler('click');
+        });
+
+        it('vertical buttons', function (){
+            new Dialog({content:'内容', horizontal:false});
+            expect($('.zui-dialog-btngroup-v').length).toBe(1);
+        });
+
+        it('vertical buttons', function (){
+            new Dialog({content:'内容', horizontal:false});
+            expect($('.zui-dialog-btngroup-v').length).toBe(1);
+            expect($('.zui-dialog-btngroup-h').length).toBe(0);
+        });
+
+        it('horizontal buttons', function (){
+            new Dialog({content:'内容', horizontal:true});
+            expect($('.zui-dialog-btngroup-v').length).toBe(0);
+            expect($('.zui-dialog-btngroup-h').length).toBe(1);
+        });
+
+    });
 });
