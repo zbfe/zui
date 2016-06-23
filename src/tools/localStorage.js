e453542d2333dacd15b6f92e5640b09bd94aea95
schoeu <quanyou1013@gmail.com>
2016-06-23 23:57:23 +0800

    update: localStorage.js

--- src/tools/localStorage.js
+++ src/tools/localStorage.js
@@ -14,38 +14,65 @@ define(function (require) {
 
     /**
      * localStorage get操作
-     * @type {string} key  标志
+     * @param {string} key  标志
      * @return {string}    值
      * */
     ls.get = function (key) {
         key = key || '';
-        return localStorage.getItem(key);
+        var value = localStorage.getItem(key);
+        if (value && $.isPlainObject(value)) {
+            try {
+                value = JSON.parse(value);
+            }
+            catch (e) {
+                // catch
+            }
+        }
+        return value;
     };
 
-    ls.set = function (key, value) {
+    /**
+     * localStorage set操作
+     * @param {string} key  标志
+     * @param {string|object} value  值
+     * */
+    ls.set = function (key, value, time) {
+        var me = this;
         if (arguments.length > 1) {
-            var temp;
             if ($.isPlainObject(value)) {
                 try {
-                    temp = JSON.parse(value);
+                    value = JSON.stringify(value);
                 }
                 catch (e) {
-                    temp = '';
                 }
             }
-            else {
-                temp = value;
-            }
-            localStorage.setItem(key, temp);
+            localStorage.setItem(key, value);
         }
+        setTimeout(function () {
+            me.remove(key);
+        }, time);
     };
 
+    /**
+     * localStorage 移除操作
+     * @param {string} key  需要移除的key值
+     * */
     ls.remove = function (key) {
         localStorage.removeItem(key);
     };
 
+    /**
+     * localStorage 清除操作
+     * */
     ls.clear = function () {
         localStorage.clear();
+    };
+
+    /**
+     * localStorage 获取localStorage长度
+     * */
+    ls.length = function () {
+        return localStorage.length;
     };
 
     return ls;
