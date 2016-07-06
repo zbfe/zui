de8a297214d45474496e12eefce2c4d449cbbfbd
xuexb <fe.xiaowu@gmail.com>
2016-07-07 07:55:58 +0800

    add: 添加延迟加载测试

---
+++ test/lazyload/dom.js
@@ -0,0 +1,17 @@
+/**
+ * @file 延迟加载dom元素测试
+ * @author xiaowu <fe.xiaowu@gmail.com>
+ */
+
+define([
+    'lazyload/dom',
+    'zepto'
+], function (Lazyload, $) {
+    'use strict';
+
+    describe('lazyload/dom', function () {
+        // options.loadElem
+
+        // 加载测试
+    });
+});
