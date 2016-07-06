87eb46ac4775bab56d9ffedf2b6ff45e6e2e3695
xuexb <fe.xiaowu@gmail.com>
2016-07-06 23:46:54 +0800

    add: 添加延迟加载图片

---
+++ src/lazyload/img.js
@@ -0,0 +1,45 @@
+/**
+ * @file 延迟加载 - 图片
+ * @author fe.xiaowu <fe.xiaowu@gmail.com>
+ * @module lazyload/img
+ */
+
+define(function (require) {
+    'use strict';
+
+    var $ = require('zepto');
+    var Base = require('./base');
+
+    var Img = Base.extend({
+        constructor: function (options) {
+            var self = this;
+            var attr;
+
+            // 初始化super
+            Img.super.constructor.call(self, $.extend({}, Img.defaults, options));
+
+            attr = self.get('attr');
+
+            // 绑定加载单个的时候加载图片
+            self.on('loaditem', function (data) {
+                var $elem = $(data.elem);
+                var src = $elem.attr(attr);
+
+                if (src) {
+                    $elem.attr('src', src).removeAttr(attr);
+                }
+            });
+        }
+    });
+
+    /**
+     * 默认参数
+     *
+     * @type {Object}
+     */
+    Img.defaults = {
+        attr: 'data-src'
+    };
+
+    return Img;
+});
