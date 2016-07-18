4767ed6e2e11e3f7cfbcd2e6e3e46831f4c800c5
liuquanyou <liuquanyou@baidu.com>
2016-07-18 23:27:48 +0800

    add: tools-hash

--- src/tools/hash.js
+++ src/tools/hash.js
@@ -6,5 +6,45 @@
 
 define(function (require) {
     'use strict';
-    
+    var hash = {};
+
+    /**
+     * hash get操作
+     * @param {string} key  标志
+     * @return {string}    值
+     * */
+    hash.get = function (key) {
+
+    };
+
+    /**
+     * hash set操作
+     * @param {string} key  标志
+     * @param {string|object} value  值
+     * @param {number} time  过期时间,可选
+     * */
+    hash.set = function (key, value, time) {
+
+    };
+
+    /**
+     * hash 移除操作
+     * @param {string} key  需要移除的key值
+     * */
+    hash.remove = function (key) {
+    };
+
+    /**
+     * hash 清除操作
+     * */
+    hash.clear = function () {
+    };
+
+    /**
+     * hash 获取hash存储值长度
+     * */
+    hash.length = function () {
+    };
+
+    return hash;
 });
