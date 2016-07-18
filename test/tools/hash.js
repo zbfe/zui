ca77fa2cbbea92b5f245ddad36cc991539ad97f1
Schoeu <quanyou1013@gmail.com>
2016-07-18 23:37:48 +0800

    Update hash.js

--- test/tools/hash.js
+++ test/tools/hash.js
@@ -1,1 +1,15 @@
+/**
+ * @file hash
+ * @author schoeu1110@gmail.com
+ */
+
+define([
+    'tools/hash'
+], function (hash) {
+    'use strict';
+
+    describe('tools/hash', function () {
+      // TODO
+    });
+});
 
